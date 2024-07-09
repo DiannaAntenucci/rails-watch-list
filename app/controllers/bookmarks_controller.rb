5cbe80f06fc47695d75995b4987229a061956837
Dianna Antenucci <diannarose9@yahoo.com>
2024-07-09 20:23:09 +0900

    watch list

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -5,18 +5,31 @@ class BookmarksController < ApplicationController
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-      if @bookmark.save
-        redirect_to_bookmark_path(@bookmark)
-      else
-        render :new, status: :unprocessable_entity
-      end
+    if @bookmark.save
+      redirect_to_bookmark_path(@bookmark)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @bookmark = Bookmark.set_bookmark
+    Bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
   end
 
   def set_list
     @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
-
+  
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id, :list_id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'bookmarks/new'
   get 'bookmarks/create'
+  root to: 'lists#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,4 +12,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :lists
+  
+
 end
